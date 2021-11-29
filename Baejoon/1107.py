a4f500b9805fdb9643afce39fd726d0a6e99db0f
arabae <arbae@inu.ac.kr>
2021-11-29 15:23:03 +0900

    modify typo of fold

--- Baejoon/1107.py
+++
@@ -1,4 +0,0 @@
-N = int(input()) # 이동하려는 채널
-M = int(input()) # 고장난 버튼 수
-broken_btn = map(int, input().split()) # 고장난 버튼 리스트
-
---
+++ Baekjoon/1107.py
@@ -0,0 +1,11 @@
+# N = int(input()) # 이동하려는 채널
+# M = int(input()) # 고장난 버튼 수
+# broken_btn = map(int, input().split()) # 고장난 버튼 리스트
+
+def test(arr):
+    arr.append([7, 8, 9])
+    return arr
+
+
+a = [[1, 2, 3], [4, 5, 6]]
+print(test(a))
